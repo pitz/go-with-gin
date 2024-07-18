fd639a54586975267ac39bc85772c90ec3e73398
Eduardo-Pitz <pitz.eduardo@gmail.com>
2024-07-18 07:53:06 +0100

    tests: adding TestAnalyseController_RemoveAnalyse

--- tests/internal/controller_test.go
+++ tests/internal/controller_test.go
@@ -77,3 +77,47 @@ func TestAnalyseController_ScheduleExecution(t *testing.T) {
 		assert.Error(t, err, "invalid ExternalID")
 	})
 }
+
+func TestAnalyseController_RemoveAnalyse(t *testing.T) {
+	mockedAdyenClient := new(fixtures.MockClient)
+	mockedTransUnionClient := new(fixtures.MockClient)
+
+	t.Run("Removing a valid Analyse", func(t *testing.T) {
+		// setup
+		controller := internal.New(mockedAdyenClient, mockedTransUnionClient)
+		controller.ScheduleExecution(validAnalyse)
+
+		// when removing a valid analyse
+		err := controller.RemoveAnalyse(validAnalyse)
+
+		// should return no errors
+		assert.NoError(t, err)
+
+		// and the analyse should no longer be present on PendingQueue()
+		assert.NotContains(t, controller.PendingQueue(), validAnalyse.ExternalId)
+	})
+
+	t.Run("Removing an already removed Analyse", func(t *testing.T) {
+		// setup
+		controller := internal.New(mockedAdyenClient, mockedTransUnionClient)
+		controller.ScheduleExecution(validAnalyse)
+		controller.RemoveAnalyse(validAnalyse)
+
+		// when trying to remove the same analyse again
+		err := controller.RemoveAnalyse(validAnalyse)
+
+		// should return an error
+		assert.Error(t, err, "analyse not found")
+	})
+
+	t.Run("Removing a non-existent Analyse", func(t *testing.T) {
+		// setup
+		controller := internal.New(mockedAdyenClient, mockedTransUnionClient)
+
+		// when trying to remove the non-existent analyse
+		err := controller.RemoveAnalyse(anotherValidAnalyse)
+
+		// should return an error
+		assert.Error(t, err, "analyse not found")
+	})
+}
